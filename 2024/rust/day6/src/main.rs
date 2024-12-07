179b53ce9e838d2e95b18953154d1b59f7d1a10d
Benn Mellinger <bennm23@gmail.com>
2024-12-07 11:20:23 -0500

    2024 day 6 optimized

--- 2024/rust/day6/src/main.rs
+++ 2024/rust/day6/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::{HashSet}, fs, hash::Hash};
 
 use utils::{get_advent_path, log, run_and_score};
 
@@ -13,7 +13,7 @@ impl Position {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Move(i32, i32);
 
 impl Move {
@@ -50,9 +50,20 @@ impl Move {
             self.1 = 0;
         }
     }
+    fn int_val(&self) -> usize {
+        if self.0 == -1 {
+            return 0;
+        } else if self.0 == 1 {
+            return 1;
+        } else if self.1 == -1 {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Guard {
     position : Position,
     movement : Move,
@@ -90,6 +101,18 @@ impl Guard {
     }
 }
 
+fn get_guard_path(guard : &mut Guard, grid : &Vec<Vec<char>>) -> HashSet<Position> {
+
+    let mut path : HashSet<Position> = HashSet::new();
+
+    while guard.make_move(&grid) {
+
+        path.insert(guard.position);
+    }
+
+    path
+}
+
 fn p1() -> i32 {
     let contents = fs::read_to_string(get_advent_path("day6.txt"))
         .expect("Could Not Parse File");
@@ -116,12 +139,7 @@ fn p1() -> i32 {
 
     print_state(guard, &grid);
 
-    let mut visited : HashSet<Position> = HashSet::new();
-
-    while guard.make_move(&grid) {
-
-        visited.insert(guard.position);
-    }
+    let visited = get_guard_path(&mut guard, &grid);
 
     visited.len() as i32
 }
@@ -153,7 +171,6 @@ fn p2() -> i32 {
 
     let size = initial_grid.len();
 
-
     let mut initial_guard = Guard{position: Position(0,0), movement: Move(-1,0)};
 
     'outer: for i in 0 .. size {
@@ -166,29 +183,29 @@ fn p2() -> i32 {
             }
         }
     }
+    let path = get_guard_path(&mut initial_guard.clone(), &initial_grid.clone());
 
     let mut sum = 0;
-    'outer: for i in 0 .. size {
-        for j in 0 .. size {
-
-            if initial_grid[i][j] == '#'  || initial_grid[i][j] == '^' {
-                continue;
-            }
-
-            let mut grid = initial_grid.clone();
-            grid[i][j] = '#';
-
-            sum += solve_grid2(&mut initial_guard.clone(), &grid);
+    let path_len = path.len();
+    
+    for pos in path {
+        if initial_grid[pos.0][pos.1] == '#'  || initial_grid[pos.0][pos.1] == '^' {
+            continue;
         }
+
+        let mut grid = initial_grid.clone();
+        grid[pos.0][pos.1] = '#';
+
+        sum += solve_grid2d(&mut initial_guard.clone(), &grid, path_len);
+        // sum += solve_grid_3d(&mut initial_guard.clone(), &grid);
     }
-
     sum
-
 }
+const SIZE : usize = 130;
 
-fn solve_grid2(guard : &mut Guard, grid : &Vec<Vec<char>>) -> i32 {
+fn solve_grid2d(guard : &mut Guard, grid : &Vec<Vec<char>>, path_len : usize) -> i32 {
 
-    let mut visited : HashSet<Position> = HashSet::new();
+    let mut visited : [[bool; SIZE]; SIZE] = [[false; SIZE]; SIZE];
 
     let mut last_pos = guard.position;
 
@@ -196,21 +213,42 @@ fn solve_grid2(guard : &mut Guard, grid : &Vec<Vec<char>>) -> i32 {
 
     while guard.make_move(&grid) {
 
-        if guard.position != last_pos && visited.contains(&guard.position) {
+        if guard.position != last_pos && visited[guard.position.0][guard.position.1] {
             loop_counter += 1;
         }
-        if loop_counter > visited.len() {
+        if loop_counter > path_len {
             return 1;
         }
-        visited.insert(guard.position);
+        visited[guard.position.0][guard.position.1] = true;
+
         last_pos = guard.position;
     }
+    0
+}
 
+fn solve_grid_3d(guard : &mut Guard, grid : &Vec<Vec<char>>) -> i32 {
+
+    let mut visited : [[[bool; 4]; SIZE]; SIZE] = [[[false; 4]; SIZE]; SIZE];
+
+    let mut last_pos = guard.position;
+
+    while guard.make_move(&grid) {
+
+        if guard.position != last_pos && visited[guard.position.0][guard.position.1][guard.movement.int_val()] {
+            return 1;
+        }
+        visited[guard.position.0][guard.position.1][guard.movement.int_val()] = true;
+
+        last_pos = guard.position;
+    }
     0
 }
 
 fn main() {
-
     run_and_score("Part 1", || { p1() });//4515, 4240 us
-    run_and_score("Part 2", || { p2() });//1309, 4367754 us
+    //1309, naive 4367754 us. 
+    //HashSet path only 764746 us.
+    //2d array path only 67579 us 59443 us after caching
+    //3d array path only 78024 us 54058 us after caching
+    run_and_score("Part 2", || { p2() });
 }
