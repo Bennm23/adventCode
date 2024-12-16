5eacee5fed0561cf2e5eade21d88fb45ec19ac99
Benn Mellinger <bennm23@gmail.com>
2024-12-15 19:36:32 -0500

    Add golang templates 2024 days 7 and 8

---
+++ 2024/go/day7/day7.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "advent/lib"
+)
+
+func main() {
+    lib.RunAndScore("Part 1", p1)
+    lib.RunAndScore("Part 2", p2)
+}
+
+func p1() int {
+    sum := 0
+
+    return sum
+}
+func p2() int {
+    sum := 0
+
+    return sum
+}
