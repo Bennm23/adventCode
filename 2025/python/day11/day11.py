a24f3a4e1b9369d4bc50f7b554d5b3733a0f195b
Bennm23 <bennm23@gmail.com>
2025-12-13 21:08:38 -0500

    2025 day 11 p2

--- 2025/python/day11/day11.py
+++ 2025/python/day11/day11.py
@@ -5,7 +5,7 @@ from lib import read_to_input
 class Device:
     def __init__(self, line: str):
         self.name = line.split(":")[0]
-        self.connections = line.split(":")[1][1:].strip().split(" ")
+        self.connections = line.split(":")[1][1:].strip().split()
     
     def is_out(self) -> bool:
         return self.connections[0] == "out"
@@ -15,85 +15,26 @@ device_map = {}
 for device in devices:
     device_map[device.name] = device
 
+@lru_cache(maxsize=None)
+def search(curr: Device, found_dac: bool, found_fft: bool) -> int:
+    if curr.is_out():
+        return 1 if found_dac and found_fft else 0
+    else:
+        if curr.name == "dac":
+            found_dac = True
+        if curr.name == "fft":
+            found_fft = True
+
+        cntr = 0
+        for conn in curr.connections:
+            cntr += search(device_map[conn], found_dac, found_fft)
+        return cntr
+
 def p1() -> int:
-    total_paths = 0
-
-    start = device_map["you"]
-
-    visited = set()
-
-    def search(curr:Device, path: str) -> int:
-        if curr.is_out():
-            return 1
-        else:
-            cntr = 0
-            for conn in curr.connections:
-                new_path = path + conn
-                if new_path not in visited:
-                    visited.add(new_path)
-                    cntr += search(device_map[conn], new_path)
-                    # visited.remove(new_path)
-            return cntr
-
-    for conn in start.connections:
-        curr = "you" + conn
-        paths = search(device_map[conn], curr)
-        total_paths += paths
-
-
-    return total_paths
-
-# @lru_cache(maxsize=None)
-def connected_to(target: str, all: set[str], visited: set[str]) -> None:
-    if target not in device_map:
-        return
-
-    if target in visited:
-        return
-    visited.add(target)
-
-    if target in device_map:
-        for conn in device_map[target].connections:
-            if conn not in all:
-                all.add(conn)
-                connected_to(conn, all, visited)
-    # device = device_map[target]
-    for device in devices:
-        if target in device.connections:
-            all.add(device.name)
-            connected_to(device.name, all, visited)
+    return search(device_map["you"], True, True)
 
 def p2() -> int:
-    total_paths = 0
+    return search(device_map["svr"], False, False)
 
-    start = device_map["svr"]
-
-    visited = set()
-
-    @lru_cache(maxsize=None)
-    def search(curr: Device, path: str) -> int:
-        if curr.is_out() and "dac" in path and "fft" in path:
-            return 1
-        else:
-            cntr = 0
-            for conn in curr.connections:
-                new_path = path + "," + conn
-                if new_path not in visited:
-                    visited.add(new_path)
-                    if conn == "out":
-                        continue
-                    cntr += search(device_map[conn], new_path)
-                    # visited.remove(new_path)
-            return cntr
-
-    for conn in start.connections:
-        print("Processing connection:", conn)
-        curr = "svr" + "," + conn
-        paths = search(device_map[conn], curr)
-        total_paths += paths
-
-
-    return total_paths
-
-# print("Part 1:", p1()) # 708
-print("Part 2:", p2()) # 
+print("Part 1:", p1()) # 708
+print("Part 2:", p2()) # 545394698933400
